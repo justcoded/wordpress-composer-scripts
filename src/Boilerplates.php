420962680e8925c387b8bb380764d3bf70544a97
jc_mzhirov <mzhirov@justcoded.co>
2017-11-20 10:38:28 +0200

    cleaned code

--- src/Boilerplates.php
+++ src/Boilerplates.php
@@ -14,14 +14,6 @@ use Composer\Script\Event;
 class Boilerplates {
 
 	/**
-	 * Replacement in theme variables
-	 *
-	 * @var array
-	 */
-	public static $replacement = array();
-
-
-	/**
 	 * Initial function for theme installation
 	 *
 	 * @param Event $event
@@ -31,12 +23,10 @@ class Boilerplates {
 		$io          = $event->getIO();
 		$arguments = $event->getArguments();
 		$args_ready = CustomComposerHelper::arguments_cleaner( $arguments );
-
 		$theme_title = $args_ready['title'];
 		$name_space  = $args_ready['namespace'];
 		$name_space  = str_replace( ' ', '', ucfirst( $name_space ) );
 		$path_to_theme_directory = $args_ready['dir'];
-
 		$theme_dir  = $args_ready['theme_slug'];
 		if ( empty( $theme_dir ) ) {
 			$theme_dir = 'default';
@@ -46,7 +36,6 @@ class Boilerplates {
 		} else {
 			$dst = 'wp-content/themes/' . $theme_dir;
 		}
-
 		$answer = '';
 		if ( false === $args_ready['silent'] ) {
 			$question = 'You creating project "'
@@ -56,94 +45,22 @@ class Boilerplates {
 			            . '" do you agree ? (yes/no)';
 			$answer = $event->getIO()->ask( $question );
 		}
-
+		$replacement = array(
+			'_jmvt_name'  => $theme_title,
+			'_jmvt'       => $args_ready['theme_slug'],
+			'Boilerplate' => $name_space,
+		);
 		if ( 'yes' === strtolower( $answer ) || 'y' === strtolower( $answer ) || '' === $answer ) {
 			$src               = 'vendor/wordpress-theme-boilerplate';
-			self::$replacement = array(
-				'_jmvt_name'  => $theme_title,
-				'_jmvt'       => $args_ready['theme_slug'],
-				'Boilerplate' => $name_space,
-			);
 			if ( is_dir( $src ) ) {
 				$dir = opendir( $src );
-				self::recursive_copy( $src, $dst );
-
-				foreach ( self::$replacement as $str_to_find => $str_to_replace ) {
-					self::search_and_replace( $dst, $str_to_find, $str_to_replace );
+				CustomComposerHelper::recursive_copy( $src, $dst );
+				foreach ( $replacement as $str_to_find => $str_to_replace ) {
+					CustomComposerHelper::search_and_replace( $dst, $str_to_find, $str_to_replace );
 				}
 			}
 		} else {
 			exit();
 		}
 	}
-
-
-	/**
-	 * @param: source folder $src
-	 * @param: destination folder $dst
-	 */
-	public static function recursive_copy( $src, $dst ) {
-		$dir = opendir( $src );
-		@mkdir( $dst );
-		while ( false !== ( $file = readdir( $dir ) ) ) {
-
-			if ( ( '.' !== $file ) && ( '..' !== $file ) ) {
-				if ( is_dir( $src . '/' . $file ) ) {
-					self::recursive_copy( $src . '/' . $file, $dst . '/' . $file );
-				} else {
-					copy( $src . '/' . $file, $dst . '/' . $file );
-				}
-			}
-		}
-
-		closedir( $dir );
-	}
-
-
-	/**
-	 * @param $dir
-	 * @param $stringsearch
-	 * @param $stringreplace
-	 *
-	 * @return array
-	 */
-	public static function search_and_replace( $dir, $stringsearch, $stringreplace ) {
-
-		$listDir = array();
-		if ( $handler = opendir( $dir ) ) {
-			while ( ( $sub = readdir( $handler ) ) !== false ) {
-				if ( $sub != "." && $sub != ".." && $sub != "Thumb.db" ) {
-					if ( is_file( $dir . "/" . $sub ) ) {
-						if ( substr_count( $sub, '.php' ) || substr_count( $sub, '.css' ) ) {
-							$getfilecontents = file_get_contents( $dir . "/" . $sub );
-							if ( substr_count( $getfilecontents, $stringsearch ) > 0 ) {
-								$replacer = str_replace( $stringsearch, $stringreplace, $getfilecontents );
-								// Let's make sure the file exists and is writable first.
-								if ( is_writable( $dir . "/" . $sub ) ) {
-									if ( ! $handle = fopen( $dir . "/" . $sub, 'w' ) ) {
-										echo "Cannot open file (" . $dir . "/" . $sub . ")";
-										exit;
-									}
-									// Write $somecontent to our opened file.
-									if ( fwrite( $handle, $replacer ) === false ) {
-										echo "Cannot write to file (" . $dir . "/" . $sub . ")";
-										exit;
-									}
-									fclose( $handle );
-								}
-							}
-						}
-						$listDir[] = $sub;
-					} elseif ( is_dir( $dir . "/" . $sub ) ) {
-						$listDir[ $sub ] = self::search_and_replace( $dir . "/" . $sub, $stringsearch, $stringreplace );
-					}
-				}
-			}
-			closedir( $handler );
-		}
-
-		return $listDir;
-	}
-
-
 }
