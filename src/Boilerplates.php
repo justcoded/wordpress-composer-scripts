ce40cedebd047225406d7b36d55043bc44b439fc
alex_prokopenko <ab.prokopenko@gmail.com>
2017-11-21 18:37:29 +0200

    Real working copy of theme creating script

--- src/Boilerplates.php
+++ src/Boilerplates.php
@@ -17,9 +17,19 @@ use JustCoded\WP\Composer\Helpers\Scripts_Helper;
 class Boilerplates {
 
 	/**
-	 * Initial function for theme installation
+	 * New theme generator based on Justcoded Theme Boilerplate (https://github.com/justcoded/wordpress-theme-boilerplate)
 	 *
-	 * @param Event $event
+	 * Usage:
+	 *      wp:theme <folder-name> [-t="My Theme"] [-ns="ClientName"] [-dir="wp-content/themes"] [-s, silent install]
+	 *
+	 * Options:
+	 *      -t          Theme name, displayed in WordPress admin panel
+	 *      -ns         Namespace to be used for theme classes
+	 *      -dir        Themes base directory. Default to 'wp-content/themes'
+	 *      -s          Silent install, setup theme without confirmation message
+	 *
+	 * @param Event $event Composer event.
+	 *
 	 * @return bool
 	 */
 	public static function theme( Event $event ) {
@@ -29,46 +39,50 @@ class Boilerplates {
 		if ( empty( $args[0] ) ) {
 			return Scripts_Helper::command_info( $io, __METHOD__, __CLASS__ );
 		}
-		// prepare data.
+
+		// Prepare data.
 		$theme       = $args[0];
 		$theme_title = Array_Helper::get_value( $args, 't', ucfirst( $theme ) );
 		$name_space  = Array_Helper::get_value( $args, 'ns', ucfirst( str_replace( '-', '_', $theme ) ) );
 		$name_space  = str_replace( ' ', '', $name_space );
 		$dir         = Array_Helper::get_value( $args, 'dir', 'wp-content/themes' );
-		$dst = rtrim( $dir, '/' ) . '/' . $theme;
-		// If there are no '-s' silent argument - get a question to user.
-		if ( empty( $args['s'] ) ) {
-			$question = 'You creating project "'
-			            . ucfirst( $theme_title )
-			            . '" on path "' . $dst
-			            . '" with namespace "' . $name_space
-			            . '" do you agree ? (yes/no)';
-			$answer   = $event->getIO()->ask( $question );
-			if ( $answer && false === strpos( strtolower( $answer ), 'y' ) ) {
-				$io->write( 'Terminating' );
+		$dir         = trim( $dir, '/' ) . '/' . $theme;
 
-				return;
+		// If there are no '-s' silent argument - get a question to user.
+		$io->write( 'You are about to create a new theme:' );
+		$io->write( "\tPath:        $dir" );
+		$io->write( "\tTitle:       $theme_title" );
+		$io->write( "\tNamespace:   $name_space\\Theme\\*" );
+
+		if ( empty( $args['s'] ) ) {
+			$answer = $io->ask( 'Do you want to continue (yes/no)? ' );
+			if ( $answer && false === strpos( strtolower( $answer ), 'y' ) ) {
+				$io->write( 'Terminating.' );
+
+				return false;
 			}
 		}
 		// Replacement array.
+		$textdomain  = str_replace( '-', '_', $theme );
 		$prefix      = str_replace( '-', '_', $theme ) . '_';
 		$replacement = array(
 			'JustCoded Theme Boilerplate' => $theme_title,
-			'boilerplate_'                => $prefix,
 			'Boilerplate\\'               => $name_space . '\\',
-			'_jmvt_name'                  => $theme_title,
+			'boilerplate_'                => $prefix,
+			"'boilerplate'"                => "'{$textdomain}'",
 		);
-		// Depending on user answer continue or stop working.
+
+		// Run copy and replace.
 		$composer = $event->getComposer();
 		$root_dir = dirname( $composer->getConfig()->get( 'vendor-dir' ) );
-		$src = $root_dir . '/vendor/wordpress-theme-boilerplate';
-		$dst = $root_dir . '/' . $dst;
+		$src      = $root_dir . '/vendor/justcoded/wordpress-theme-boilerplate';
+		$dst      = $root_dir . '/' . $dir;
 		if ( opendir( $src ) ) {
 			File_System_Helper::copy_dir( $src, $dst );
 			File_System_Helper::search_and_replace( $dst, $replacement );
 		} else {
 			$event->getIO()->write( 'There are was an error before start copying theme files' );
 		}
-		$event->getIO()->write( 'The task has been created!' );
+		$event->getIO()->write( 'Theme has been created!' );
 	}
 }
